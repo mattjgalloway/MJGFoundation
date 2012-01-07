04a70206fab0521b3b36c96af984cbe7aa09268d
Matt Galloway <matt@galloway.me.uk>
2012-01-07 11:58:54 +0000

    Add a simple stack object

---
+++ Source/Model/MJGStack.h
@@ -0,0 +1,22 @@
+//
+//  MJGStack.h
+//  MJGFoundation
+//
+//  Created by Matt Galloway on 06/01/2012.
+//  Copyright (c) 2012 Matt Galloway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MJGStack : NSObject
+
+@property (nonatomic, assign, readonly) NSUInteger count;
+
+- (id)initWithArray:(NSArray*)array;
+
+- (void)pushObject:(id)object;
+- (void)pushObjects:(NSArray*)objects;
+- (id)popObject;
+- (id)peekObject;
+
+@end
---
+++ Source/Model/MJGStack.m
@@ -0,0 +1,73 @@
+//
+//  MJGStack.m
+//  MJGFoundation
+//
+//  Created by Matt Galloway on 06/01/2012.
+//  Copyright (c) 2012 Matt Galloway. All rights reserved.
+//
+
+#import "MJGStack.h"
+
+@interface MJGStack ()
+@property (nonatomic, strong) NSMutableArray *objects;
+@end
+
+@implementation MJGStack
+
+@synthesize objects = _objects;
+
+#pragma mark -
+
+- (id)init {
+    if ((self = [self initWithArray:nil])) {
+    }
+    return self;
+}
+
+- (id)initWithArray:(NSArray*)array {
+    if ((self = [super init])) {
+        _objects = [[NSMutableArray alloc] initWithArray:array];
+    }
+    return self;
+}
+
+
+#pragma mark - Custom accessors
+
+- (NSUInteger)count {
+    return _objects.count;
+}
+
+
+#pragma mark -
+
+- (void)pushObject:(id)object {
+    if (object) {
+        [_objects addObject:object];
+    }
+}
+
+- (void)pushObjects:(NSArray*)objects {
+    for (id object in objects) {
+        [self pushObject:object];
+    }
+}
+
+- (id)popObject {
+    if (_objects.count > 0) {
+        id object = [_objects objectAtIndex:(_objects.count - 1)];
+        [_objects removeLastObject];
+        return object;
+    }
+    return nil;
+}
+
+- (id)peekObject {
+    if (_objects.count > 0) {
+        id object = [_objects objectAtIndex:(_objects.count - 1)];
+        return object;
+    }
+    return nil;
+}
+
+@end
