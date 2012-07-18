c99fa38521b2d7500ccea8431d43051db9483d14
Matt Galloway <matt@galloway.me.uk>
2012-07-18 10:01:38 +0100

    Add ARC requirement

--- Source/Model/MJGSortedMutableArray.m
+++ Source/Model/MJGSortedMutableArray.m
@@ -6,6 +6,10 @@
 //  Copyright (c) 2012 Matt Galloway. All rights reserved.
 //
 
+#if ! __has_feature(objc_arc)
+#error This file requires ARC to be enabled. Either enable ARC for the entire project or use -fobjc-arc flag.
+#endif
+
 #import "MJGSortedMutableArray.h"
 
 @interface MJGSortedMutableArray ()
