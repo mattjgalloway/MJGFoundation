319dd62ed8f7d71beab1cf17f4fcbbba010cf05a
Matt Galloway <matt@galloway.me.uk>
2012-07-18 08:54:42 +0100

    Just use blocks rather than yet another object

--- Source/Model/MJGSortedMutableArray.m
+++ Source/Model/MJGSortedMutableArray.m
@@ -8,46 +8,15 @@
 
 #import "MJGSortedMutableArray.h"
 
-@interface MJGSortedMutableArrayHelper : NSObject {
-}
-- (id)init;
-- (NSComparisonResult)compareObject:(id)object1 toObject:(id)object2;
-@end
-
-@interface MJGSortedMutableArrayHelperDescriptors : MJGSortedMutableArrayHelper {
-    NSArray *_descriptors;
-}
-- (id)initWithDescriptors:(NSArray*)descriptors;
-@end
-
-@interface MJGSortedMutableArrayHelperComparator : MJGSortedMutableArrayHelper {
-    NSComparator _comparator;
-}
-- (id)initWithComparator:(NSComparator)comparator;
-@end
-
-@interface MJGSortedMutableArrayHelperFunction : MJGSortedMutableArrayHelper {
-    NSInteger (*_compare)(id, id, void *);
-    void *_context;
-}
-- (id)initWithFunction:(NSInteger (*)(id, id, void *))compare context:(void *)context;
-@end
-
-@interface MJGSortedMutableArrayHelperSelector : MJGSortedMutableArrayHelper {
-    SEL _selector;
-}
-- (id)initWithSelector:(SEL)selector;
-@end
-
 @interface MJGSortedMutableArray ()
 @property (nonatomic, strong) NSMutableArray *backingArray;
-@property (nonatomic, strong) MJGSortedMutableArrayHelper *helper;
+@property (nonatomic, strong) NSComparator comparator;
 @end
 
 @implementation MJGSortedMutableArray
 
 @synthesize backingArray = _backingArray;
-@synthesize helper = _helper;
+@synthesize comparator = _comparator;
 
 #pragma mark -
 
@@ -60,28 +29,40 @@
 
 - (id)initWithDescriptors:(NSArray*)descriptors {
     if ((self = [self init])) {
-        self.helper = [[MJGSortedMutableArrayHelperDescriptors alloc] initWithDescriptors:descriptors];
+        self.comparator = ^NSComparisonResult(id obj1, id obj2) {
+            for (NSSortDescriptor *descriptor in descriptors) {
+                NSComparisonResult result = [descriptor compareObject:obj1 toObject:obj2];
+                if (result != NSOrderedSame) {
+                    return result;
+                }
+            }
+            return NSOrderedSame;
+        };
     }
     return self;
 }
 
 - (id)initWithComparator:(NSComparator)comparator {
     if ((self = [self init])) {
-        self.helper = [[MJGSortedMutableArrayHelperComparator alloc] initWithComparator:comparator];
+        self.comparator = comparator;
     }
     return self;
 }
 
 - (id)initWithFunction:(NSInteger (*)(id, id, void *))compare context:(void *)context {
     if ((self = [self init])) {
-        self.helper = [[MJGSortedMutableArrayHelperFunction alloc] initWithFunction:compare context:context];
+        self.comparator = ^NSComparisonResult(id obj1, id obj2) {
+            return compare(obj1, obj2, context);
+        };
     }
     return self;
 }
 
 - (id)initWithSelector:(SEL)selector {
     if ((self = [self init])) {
-        self.helper = [[MJGSortedMutableArrayHelperSelector alloc] initWithSelector:selector];
+        self.comparator = ^NSComparisonResult(id obj1, id obj2) {
+            return [obj1 performSelector:selector withObject:obj2];
+        };
     }
     return self;
 }
@@ -92,7 +73,7 @@
 - (NSUInteger)addObject:(id)obj {
     __block NSUInteger addedIndex = NSNotFound;
     [_backingArray enumerateObjectsUsingBlock:^(id obj2, NSUInteger idx, BOOL *stop) {
-        NSComparisonResult result = [_helper compareObject:obj toObject:obj2];
+        NSComparisonResult result = _comparator(obj, obj2);
         if (result != NSOrderedDescending) {
             addedIndex = idx;
             [_backingArray insertObject:obj atIndex:addedIndex];
@@ -119,71 +100,4 @@
     return [_backingArray copy];
 }
 
-@end
-
-@implementation MJGSortedMutableArrayHelper
-- (id)init {
-    if ((self = [super init])) {
-    }
-    return self;
-}
-- (NSComparisonResult)compareObject:(id)object1 toObject:(id)object2 {
-    return NSOrderedSame;
-}
-@end
-
-@implementation MJGSortedMutableArrayHelperDescriptors
-- (id)initWithDescriptors:(NSArray *)descriptors {
-    if ((self = [super init])) {
-        _descriptors = descriptors;
-    }
-    return self;
-}
-- (NSComparisonResult)compareObject:(id)object1 toObject:(id)object2 {
-    NSComparisonResult result = NSOrderedSame;
-    for (NSSortDescriptor *descriptor in _descriptors) {
-        result = [descriptor compareObject:object1 toObject:object2];
-        if (result != NSOrderedSame) {
-            return result;
-        }
-    }
-    return result;
-}
-@end
-
-@implementation MJGSortedMutableArrayHelperComparator
-- (id)initWithComparator:(NSComparator)comparator {
-    if ((self = [super init])) {
-        _comparator = [comparator copy];
-    }
-    return self;
-}
-- (NSComparisonResult)compareObject:(id)object1 toObject:(id)object2 {
-    return _comparator(object1, object2);
-}
-@end
-
-@implementation MJGSortedMutableArrayHelperFunction
-- (id)initWithFunction:(NSInteger (*)(__strong id, __strong id, void *))compare context:(void *)context {
-    if ((self = [super init])) {
-        _compare = compare;
-        _context = context;
-    }
-    return self;
-}
-- (NSComparisonResult)compareObject:(id)object1 toObject:(id)object2 {
-    return _compare(object1, object2, _context);
-}
-@end
-
-@implementation MJGSortedMutableArrayHelperSelector
-- (id)initWithSelector:(SEL)selector {
-    if ((self = [super init])) {
-        _selector = selector;
-    }
-    return self;
-}
-- (NSComparisonResult)compareObject:(id)object1 toObject:(id)object2 {
-    return [object1 performSelector:_selector withObject:object2];
-}
 @end
